9d553455ea578fe616fc8b288189795ff9e62133
Juan Esteban Betancourt Galindo <juanes0104@outlook.com>
2019-07-19 13:11:06 -0500

    Cambios Paula Ochoa
    Cambios realizados por Paula Ochoa

---
+++ funciones del administrador/modelo/persona.modelo.php
@@ -0,0 +1,92 @@
+<?php
+    require 'conexion.php';
+
+    class Persona{
+        
+        public function ConsultarTodo(){
+            $conexion = new conexion();
+            $stmt = $conexion->prepare("SELECT * FROM promociones")
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_OBJ);
+        }
+        public function ConsultarPorId($idPromociones)
+        $conexion = new conexion();
+        $stmt = $conexion->prepare("SELECT * FROM promociones WHERE idPromciones = :idPromociones");
+        $stmt->bindValue(":idPromciones", $idPromociones, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_OBJ);
+
+        public function ConsultarTodo(){
+            $conexion = new Conexion();
+            $stmt = $conexion->prepare("SELECT * FROM promociones");
+        }
+
+        public function ConsultarPorId($idPromociones){
+
+        }
+
+        public function Guardar($nombres , $contenido , $precio , $acciones )
+
+        $conexion = new Conexion();
+        $stmt = $conexion->prepare("INSERT INTO 'rba'.'persona'
+                                                    ('nombres',
+                                                    'contenido',
+                                                    'precio',
+                                                    'acciones',)
+                                            VALUES(:nombres,
+                                            :contenido,
+                                            :precio,
+                                            :acciones);
+                                            ");
+    $stmt->bindValue(":nombres", $nombres, PDO::PARAM_STR);
+    $stmt->bindValue(":contenido", $contenido, PDO::PARAM_STR);
+    $stmt->bindValue(":precio", $precio, PDO::PARAM_STR);
+    $stmt->bindValue(":acciones", $acciones, PDO::PARAM_STR);
+
+    if($stmt->execute()){
+        return "OK"
+    }else{
+        return "Error: se ha generado un error al guardar la informacion";
+
+    }
+    }
+    public function Modificar( $idPromociones , $nombres , $contenido , $precio , $acciones )
+
+    $conexion = new Conexion();
+    $stmt = $conexion->prepare("UPDATE `promociones` 
+                                SET     `nombredepromocion`=`:nombredepromocion`,
+                                        `contenido`= :contenido',
+                                        `precio`= :precio' 
+                                         
+                                 WHERE 'idPromocion'= :idPromocion';
+                                 ")
+$stmt->bindValue(":nombres", $nombres, PDO::PARAM_STR);
+$stmt->bindValue(":contenido", $contenido, PDO::PARAM_STR);
+$stmt->bindValue(":precio", $precio, PDO::PARAM_STR);
+$stmt->bindValue(":acciones", $acciones, PDO::PARAM_STR);
+$stmt->bindValue(":idPromocion", $idPromociones, PDO::PARAM_INT);
+
+if($stmt->execute()){
+    return "OK"
+}else{
+    return "Error: se ha generado un error al modificar la informacion";
+    
+        }
+
+        public function Eliminar ( $idPromociones )
+
+        $conexion = new Conexion();
+        $stmt = $conexion->prepare("DELETE FROM persona WHERE idPromcion = idPromocion ")
+        $stmt->bindValue(":idPromocion", $idPromociones, PDO::PARAM_INT);
+    
+    if($stmt->execute()){
+        return "OK"
+    }else{
+        return "Error: se ha generado un error al eliminar la informacion";
+        
+            }
+    
+      
+
+
+?>
