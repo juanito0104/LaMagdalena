0e10b316b9e717765f51c2ca521e8c079f0bf8b7
lamagdalenares <restaurantelamagdalena7@gmail.com>
2019-08-06 15:40:07 -0500

    login nuevo
    beta: "loguin nuevo"

--- app/modelo/loguear.php
+++ app/modelo/loguear.php
@@ -1,21 +1,43 @@
 <?php
 
+require 'configuracion.php';
+
 session_start();
 
-require 'conexion.php'
+$conexion = mysqli_connect(servidor, usuario, '', base_datos);
 
-$usuario = $_POST['usuario']
-$password = $_POST['password']
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
 
-$q = "SELECT COUNT(*) as contar from login where usuario = '$usuario' and contraseña = '$password'";
+$sql = "SELECT * from login where usuario = '$usuario'";
 
-$consulta = mysqli_query($conexion, $q);
-$array = mysqli_fetch_array($consulta);
+$stmt = $conexion->query($sql);
 
-if($array['contar']>0){
-    header("location: ../../index.html")
+if(mysqli_num_rows($stmt) > 0){
+    $registro = $stmt->fetch_object();
+    if($registro->contrasena != $password){
+        echo "
+            <script>
+                alert('Contraseña incorrecta');
+                window.location = '../../vista/login.html';
+            </script>
+        ";
+    }
+
+    echo "
+        <script>
+            alert('Sesión iniciada con éxito');
+            window.location = '../../vista/login.html';
+        </script>
+    ";
 }else{
-    echo "Datos Incorrectos"
+    echo "
+        <script>
+            alert('Usuario no existe');
+            window.location = '../../vista/login.html';
+        </script>
+    ";
 }
+
 
 ?>
